130cae3a2e35c5366e294efeb94ed9d233234e15
LucMazarJR <159916665+LucMazarJR@users.noreply.github.com>
2025-07-08 13:48:39 -0300

    Add files via upload

---
+++ ex6.py
@@ -0,0 +1,15 @@
+"""1. Reescreva o sistema de cadastro de alunos utilizando como
+estruturas de dados dicionários e tuplas ao invés de listas.
+
+disciplinas[sigla]=[nome, carga_horaria] #string e lista
+dic_alunos[(RA, nome)] = (data_nasc,[e-mails],[ [disc1, nota1] ,....,
+[discN, notaN] ] ) # tupla e tupla
+
+Menu:
+1-Adicionar Aluno
+2-Alterar Aluno
+3-Remover Aluno
+4-Consultar um Aluno pela chave completa
+5-Mostrar todos os alunos
+6-Adicionar disciplinas e notas ao Aluno
+7-Fim"""
